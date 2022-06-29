7c4cbedcacb83b3c580b9854f587c0b44c11c769
Septian Adhitya <dsrisk911@gmail.com>
2022-06-29 15:09:20 +0700

    add belajar-nodejs

---
+++ belajar-nodejs/index.js
@@ -0,0 +1,8 @@
+// const nama = "Septian Adhitya";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+const cetakNama = require("./coba");
+
+// console.log("Hello WPU");
+console.log(cetakNama("Septian"));
