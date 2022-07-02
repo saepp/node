b550377457470ee5523ff25589881840f04151d9
Septian Adhitya <dsrisk911@gmail.com>
2022-07-02 13:51:02 +0700

    update contact-app

--- belajar-nodejs/contact-app/app.js
+++ belajar-nodejs/contact-app/app.js
@@ -1,11 +1,29 @@
-const { tulisPertanyaan, simpanContact } = require("./contacts");
+const yargs = require("yargs");
+const { simpanContact } = require("./contacts");
 
-const main = async () => {
-  const nama = await tulisPertanyaan("Masukkan nama anda : ");
-  const email = await tulisPertanyaan("Masukkan email anda : ");
-  const noHP = await tulisPertanyaan("Masukkan No HP anda : ");
+yargs.command({
+  command: "add",
+  describe: "Menambahkan contact baru",
+  builder: {
+    nama: {
+      describe: "Nama lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+    noHP: {
+      describe: "No Handphone",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    simpanContact(argv.nama, argv.email, argv.noHP);
+  },
+});
 
-  simpanContact(nama, email, noHP);
-};
-
-main();
+yargs.parse();
--- belajar-nodejs/contact-app/contacts.js
+++ belajar-nodejs/contact-app/contacts.js
@@ -1,10 +1,6 @@
 const fs = require("fs");
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const chalk = require("chalk");
+const validator = require("validator");
 
 // membuat folder data
 const dirPath = "./data";
@@ -18,22 +14,37 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, reject) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama);
-    });
-  });
-};
-
 const simpanContact = (nama, email, noHP) => {
   const contact = { nama, email, noHP };
   const fileBuffer = fs.readFileSync("data/contacts.json");
   const contacts = JSON.parse(fileBuffer);
+
+  // cek duplikat
+  const duplikat = contacts.find((contact) => contact.nama === nama);
+  if (duplikat) {
+    console.log(
+      chalk.red.inverse.bold("Contact sudah terdaftar, gunakan nama lain!")
+    );
+    return false;
+  }
+
+  // cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold("Email tidak valid!"));
+      return false;
+    }
+  }
+
+  // cek no HP
+  if (!validator.isMobilePhone(noHP, "id-ID")) {
+    console.log(chalk.red.inverse.bold("Nomor HP tidak valid!"));
+    return false;
+  }
+
   contacts.push(contact);
   fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-  console.log("Terimakasih sudah memasukkan data!");
-  rl.close();
+  console.log(chalk.green.inverse.bold("Terimakasih sudah memasukkan data!"));
 };
 
-module.exports = { tulisPertanyaan, simpanContact };
+module.exports = { simpanContact };
